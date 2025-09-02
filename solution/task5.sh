4e544678be027cdbfd6d2a94755fdeee503ac531
garagulman <akgaragulova@gmail.com>
2025-09-02 15:23:39 +0700

    task5

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,2 +1,2 @@
-sed -E 's/(.*) (.*) (.*)?,(d*)/\2 \1, \4/' data/students.csv > data/students.txt
 sed -E 's/(.*) (.*)?,(d*)/\2 \1, \3/' data/students.csv > data/students.txt
+sed -E 's/(.*) (.*) (.*)?,(d*)/\2 \1, \4/' data/students.csv > data/students.txt
